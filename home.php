e29b800e00d529ca2ceddbdd3e84eacd1b58fe38
Ariefjnrzky <144106743+Ariefjnrzky@users.noreply.github.com>
2024-10-15 23:01:17 +0700

    Add files via upload

---
+++ home.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<body>
+
+<h1>Selamat Datang di Web Saya</h1>
+
+
+<table style="width:100%">
+    <tr>
+        <th><a href="home.html">Home</a></th> 
+      <th><a href="tentang.html">About</a></th>
+      <th><a href="kontak.html">Kontak</a></th>
+    </tr>
+</table>
+<u>
+
+</body>
+</html>
---
+++ konfirmasi_kontak.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo 'Konfirmasi Kontak'; ?></title>
+</head>
+<body>
+    <h1><?php echo 'KONFIRMASI KONTAK'; ?></h1>
+
+    <?php
+        $nama = $_GET["nama"];
+        $url = $_GET["url"];
+        $hobi = $_GET["hobi"];
+        $jenisKelamin = $_GET["jenis_kelamin"];
+
+        echo '
+        Nama Anda adalah '.$nama.'
+        <br>URL yang Anda masukkan '.$url.'
+        <br>Hobi Anda adalah '.$hobi.'
+        <br>Jenis kelamin Anda adalah '.$jenisKelamin.'';
+
+        
+    ?>
+</body>
+</html>
